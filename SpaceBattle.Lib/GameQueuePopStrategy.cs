8795075642fddc941323df091cdf5996cfecf873
Ermokhin Aleksandr <ermokhin143@gmail.com>
2023-06-07 08:40:28 +0600

    asd

--- SpaceBattle.Lib/GameQueuePopStrategy.cs
+++ SpaceBattle.Lib/GameQueuePopStrategy.cs
@@ -2,7 +2,7 @@ namespace SpaceBattle.Lib;
 
 public class GameQueuePopStrategy : IStrategy
 {
-    public object Execute(params object[] param)
+    public object RunStrategy(params object[] param)
     {
         Queue<ICommand> commandQueue = (Queue<ICommand>) param[0];
         return (ICommand) commandQueue.Dequeue();
